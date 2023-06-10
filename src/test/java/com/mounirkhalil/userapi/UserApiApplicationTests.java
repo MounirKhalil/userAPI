132bf70f955adef494bd11d2e74a7045dbfd273d
Mounir Khalil <mmk113@mail.aub.edu>
2023-06-10 21:31:38 +0300

    First Commit

---
+++ src/test/java/com/mounirkhalil/userapi/UserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mounirkhalil.userapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
